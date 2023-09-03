b3f6d57815a0020c3f94ff677eb4aecd6e196cf8
gbstudentDz <111184107+gbstudentDz@users.noreply.github.com>
2023-09-04 04:46:03 +0600

    5.1.1

---
+++ src/main/java/orgx/DataForParsing.java
@@ -0,0 +1,9 @@
+package orgx;
+
+public class DataForParsing {
+
+    public static class Regex {
+        public static String PRICE = "^((\\d+ )+руб.)$";
+    }
+
+}
--- src/test/java/orgx/PageUtils.java
+++ src/test/java/orgx/PageUtils.java
@@ -1,4 +1,47 @@
 package orgx;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 public class PageUtils {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private Actions actions;
+
+    public PageUtils(WebDriver driver, WebDriverWait wait, Actions actions) {
+        this.driver = driver;
+        this.wait = wait;
+        this.actions = actions;
+    }
+
+    @Step("Ожидание появления эдемента {0}")
+    public void wait(By locator) {
+        wait.until(visibilityOfElementLocated(locator));
+    }
+
+    @Step("Нажимаем на элемент")
+    public void click(By locator, int afterDelayOfSeconds) throws InterruptedException {
+        wait(locator);
+        WebElement element = driver.findElement(locator);
+        actions.moveToElement(element);
+        actions.perform();
+        element.click();
+        sleep(afterDelayOfSeconds);
+    }
+
+    @Step("Ввод текста под Shadow root")
+    public void inputWord(WebElement element, String word) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].value='"+word+"'", element);
+        element.sendKeys(Keys.BACK_SPACE);
+    }
+
+    private void sleep(int seconds) throws InterruptedException {
+        Thread.sleep(seconds * 1000);
+    }
 }
