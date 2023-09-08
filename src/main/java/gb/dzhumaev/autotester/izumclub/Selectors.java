609fffe3ab347726f77b903e0c5d371702ab23b4
gbstudentDz <111184107+gbstudentDz@users.noreply.github.com>
2023-09-08 22:19:08 +0600

    5.7.1

--- src/main/java/gb/dzhumaev/autotester/izumclub/Selectors.java
+++ src/main/java/gb/dzhumaev/autotester/izumclub/Selectors.java
@@ -38,20 +38,20 @@ public class Selectors {
     public static final SelectorInfo FAVORIVE_ON_CARD_2
             = new SelectorInfo(CARD_2.s() + " .heart_icon", "Иконка избранного в карточке товара №2");
     public static final SelectorInfo POPUP_SHOW_AFTER_FILTER
-            = new SelectorInfo(" .bx-filter-popup-result a[target]", "Показать в попапе");
+            = new SelectorInfo(" .bx-filter-popup-result a[target]", "\"Показать\" в попапе");
     public static final SelectorInfo POPUP_WINDOW
             = new SelectorInfo(" .popup-window[style^='display: block']", "____");
     public static final SelectorInfo POPUP_WINDOW_CLOSE
             = new SelectorInfo(POPUP_WINDOW.s()
-            + " .popup-window-close-icon", "Крестик закрытия в модальном окне");
+            + " .popup-window-close-icon", "крестик закрытия в модальном окне");
     public static final SelectorInfo PRICE_FILTER_BLOCK
             = new SelectorInfo(" #filter__form .block-with-two-input", "____");
     public static final SelectorInfo PRICE_MAX_IN_FILTER
             = new SelectorInfo(PRICE_FILTER_BLOCK.s()
-            + " input#arrFilter_P2_MAX", "поле ввода максимальной цены");
+            + " input#arrFilter_P2_MAX", "Поле ввода максимальной цены");
     public static final SelectorInfo PRICE_MIN_IN_FILTER
             = new SelectorInfo(PRICE_FILTER_BLOCK.s()
-            + " input#arrFilter_P2_MIN", "поле ввода минимальной цены");
+            + " input#arrFilter_P2_MIN", "Поле ввода минимальной цены");
     public static final SelectorInfo PRICE_ON_CARD = new SelectorInfo(" .price", "____");
     public static final SelectorInfo SORT_BLOCK
             = new SelectorInfo(" #sort-and-show-form .left-block .cs-select[tabindex='0']", "Раскрывющийся список сортировки");
@@ -64,4 +64,12 @@ public class Selectors {
     public static final SelectorInfo TAG_1_ON_HEADER
             = new SelectorInfo(TAGS_BLOCK_ON_HEADER.s() + " li:nth-child(1)>a", "Тэг №1");
     public static final SelectorInfo TITLE_CATEGORY_PAGE = new SelectorInfo(" .page_title h1", "____");
+
+    public static final SelectorInfo DETAIL_PAGE_FAST_ORDER = new SelectorInfo("a.fast_order", "Кнопка \"Быстрый заказ\"");
+    public static final SelectorInfo FORM_FAST_ORDER = new SelectorInfo("#fast_order_form", "____");
+    public static final SelectorInfo FORM_FAST_ORDER_CITY = new SelectorInfo("#CITY", "Поле ввода города");
+    public static final SelectorInfo FORM_FAST_ORDER_FULLNAME = new SelectorInfo("#FIO", "Поле ввода ФИО");
+    public static final SelectorInfo FORM_FAST_ORDER_PHONE = new SelectorInfo("#PHONE", "Поле ввода телефона");
+    public static final SelectorInfo FORM_FAST_ORDER_EMAIL = new SelectorInfo("#EMAIL", "Поле ввода email");
+    public static final SelectorInfo FORM_FAST_ORDER_SUBMIT = new SelectorInfo("button.btn-blue[type='submit']", "кнопка \"Заказать\"");
 }
--- src/test/java/gb/dzhumaev/autotester/izumclub/FastOrderTest.java
+++ src/test/java/gb/dzhumaev/autotester/izumclub/FastOrderTest.java
@@ -1,7 +1,6 @@
 package gb.dzhumaev.autotester.izumclub;
 
 import gb.dzhumaev.autotester.PageUtils;
-import gb.dzhumaev.autotester.SelectorInfo;
 import gb.dzhumaev.autotester.TestCongicuration;
 import gb.dzhumaev.autotester.TestListener;
 import io.qameta.allure.Epic;
@@ -14,10 +13,11 @@ import org.openqa.selenium.WebElement;
 
 import static gb.dzhumaev.autotester.PageUtils.click;
 import static gb.dzhumaev.autotester.PageUtils.sendKeys;
-import static gb.dzhumaev.autotester.izumclub.Selectors.CARD_1;
+import static gb.dzhumaev.autotester.izumclub.Selectors.*;
 import static gb.dzhumaev.autotester.izumclub.Urls.URL_CATEGORY_SMARTPHONES;
 import static gb.dzhumaev.autotester.izumclub.Urls.URL_HOME;
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Epic("Тестирование UI сайта " + URL_HOME)
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class FastOrderTest extends TestCongicuration {
 
     private final String correctTelNumber = "+79093146051";
-    private final String incorrectTelephoneNumber = "+79O93146O51";
+    private final String incorrectTelNumber = "+79O!93146O51";
     private final String correctCityName = "Москва";
     private final String incorrectCityName = "$Москва";
     private final String correctFullName = "Иванов Иван Иванович";
@@ -35,17 +35,138 @@ public class FastOrderTest extends TestCongicuration {
     private final String incorrectEmail = "example&mail.com";
 
     @Test
-    @DisplayName("Ве поля заполнены валидными значениями")
+    @DisplayName("Форма быстрого заказа: вcе обязательные поля заполнены валидными значениями")
     public void t1() throws InterruptedException {
         PageUtils.open(URL_CATEGORY_SMARTPHONES);
         click(CARD_1);
-        click(new SelectorInfo("a.fast_order", "Кнопка быстрого заказа"));
-        sendKeys(new SelectorInfo("#CITY", "поле ввода города"), correctCityName);
-        sendKeys(new SelectorInfo("#FIO", "поле ввода ФИО"), correctFullName);
-        sendKeys(new SelectorInfo("#PHONE", "поле ввода телефона"), correctTelNumber);
-        sendKeys(new SelectorInfo("#EMAIL", "поле ввода email"), correctEmail);
-        click(new SelectorInfo("button.btn-blue[type='submit']", "кнопка Заказать"));
-        WebElement element = PageUtils.wait(By.cssSelector("#fast_order_form"));
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
         assertTrue(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: поле ввода города пустое")
+    public void t2() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        //sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: поле ФИО города пустое")
+    public void t3() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        //sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: поле ввода номера пустое")
+    public void t4() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        //sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: поле ввода email пустое")
+    public void t5() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        //sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: в поле ввода города введено некоррекное значение")
+    public void t6() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, incorrectCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: в поле ввода ФИО введено некоррекное значение")
+    public void t7() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, incorrectFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: в поле ввода телефона введено некоррекное значение")
+    public void t8() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, incorrectTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, correctEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
+    }
+
+    @Test
+    @DisplayName("Форма быстрого заказа: в поле ввода email введено некоррекное значение")
+    public void t9() throws InterruptedException {
+        PageUtils.open(URL_CATEGORY_SMARTPHONES);
+        click(CARD_1);
+        click(DETAIL_PAGE_FAST_ORDER);
+        sendKeys(FORM_FAST_ORDER_CITY, correctCityName);
+        sendKeys(FORM_FAST_ORDER_FULLNAME, correctFullName);
+        sendKeys(FORM_FAST_ORDER_PHONE, correctTelNumber);
+        sendKeys(FORM_FAST_ORDER_EMAIL, incorrectEmail);
+        click(FORM_FAST_ORDER_SUBMIT);
+        WebElement element = PageUtils.wait(By.cssSelector(FORM_FAST_ORDER.s()));
+        assertFalse(asList(element.getAttribute("class").split(" ")).contains("load"));
     }
 }
